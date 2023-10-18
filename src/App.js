992c66187476ec4cb4fa5563e16337554a329f8c
Prince Ahmed <pa99@duke.edu>
2023-10-18 00:05:35 -0400

    using mapbox

--- src/App.js
+++ src/App.js
@@ -1,27 +1,73 @@
-import React from 'react';
-import starlinkLogo from './images/starlink_logo.svg';
-import { Layout } from 'antd';
-import Main from'./components/Main';
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import * as parkDate from "./data/skateboard-parks.json";
 
-const { Header, Footer, Content } = Layout;
+export default function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 45.4211,
+    longitude: -75.6903,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10
+  });
+  const [selectedPark, setSelectedPark] = useState(null);
 
-function App() {
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === "Escape") {
+        setSelectedPark(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
+
   return (
-    <Layout>
-      <Header>
-        <img src={starlinkLogo} className="App-logo" alt="logo" />
-        <span className="title">
-          StarLink Tracker
-        </span>
-      </Header>
-      <Content>
-        <Main/>
-      </Content>
-      <Footer>
-      ©2020 StarLink Tracker. All Rights Reserved. Website Made by Lucie Wang.
-      </Footer>
-    </Layout>
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/leighhalliday/cjufmjn1r2kic1fl9wxg7u1l4"
+        onViewportChange={viewport => {
+          setViewport(viewport);
+        }}
+      >
+        {parkDate.features.map(park => (
+          <Marker
+            key={park.properties.PARK_ID}
+            latitude={park.geometry.coordinates[1]}
+            longitude={park.geometry.coordinates[0]}
+          >
+            <button
+              className="marker-btn"
+              onClick={e => {
+                e.preventDefault();
+                setSelectedPark(park);
+              }}
+            >
+              <img src="/skateboarding.svg" alt="Skate Park Icon" />
+            </button>
+          </Marker>
+        ))}
+
+        {selectedPark ? (
+          <Popup
+            latitude={selectedPark.geometry.coordinates[1]}
+            longitude={selectedPark.geometry.coordinates[0]}
+            onClose={() => {
+              setSelectedPark(null);
+            }}
+          >
+            <div>
+              <h2>{selectedPark.properties.NAME}</h2>
+              <p>{selectedPark.properties.DESCRIPTIO}</p>
+            </div>
+          </Popup>
+        ) : null}
+      </ReactMapGL>
+    </div>
   );
 }
-
-export default App;
